e4fa247c0865fcf245799790ba3429856eb6dfa6
carlos15983 <carlos15983@gmail.com>
2020-05-29 06:37:03 -0700

    Front end addition

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('MongoDB connected');
+  } catch (e) {
+    {
+      errors: errors.array();
+    }
+    console.log(e.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
